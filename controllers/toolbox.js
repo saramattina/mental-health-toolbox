c174480e45f4e24624366b8522003dd14f1e8183
Sara Mattina <smattina@gmail.com>
2025-09-10 14:35:18 -0400

    create tools/show.ejs

--- controllers/toolbox.js
+++ controllers/toolbox.js
@@ -3,11 +3,10 @@ const router = express.Router();
 
 const UserTool = require("../models/userTool.js");
 const Tool = require("../models/tool.js");
-const Emotion = require("../models/emotion.js");
+// const Emotion = require("../models/emotion.js");
 
 router.get("/", async (req, res) => {
    const toolData = await Tool.find({});
-   console.log(toolData);
    res.render("tools/index.ejs", { tools: toolData});
 });
 
@@ -16,24 +15,37 @@ router.get("/new", (req, res) => {
    res.render("tools/new.ejs", { tools: toolData });
 })
 
-
-
-
-
 router.get("/:toolId", async (req, res) => {
    try {
-      const tool = await Tool.findById(req.params.toolId);
-      const userTool = await UserTool.findOne({
-         user: req.session.user._id,
-         tool: req.params.toolId,
-      }).populate();
-      res.render("tools/show.ejs", { tool, userTool });
+      // const currentUser = await User.findById(req.session.user._id);
+      const toolData = await Tool.findById(req.params.toolId);
+      console.log("tool:", toolData);
+      res.render("tools/show.ejs", { tool: toolData });
    } catch (error) {
       console.log(error);
       res.redirect("/toolbox");
    }
 });
 
+
+
+
+
+
+
+// router.get("/:toolId", async (req, res) => {
+//    try {
+//       const tool = await Tool.findById(req.params.toolId);
+//       const userTool = await UserTool.findOne({
+//          user: req.session.user._id,
+//          tool: req.params.toolId,
+//       }).populate();
+//       res.render("tools/show.ejs", { tool, userTool });
+//    } catch (error) {
+//       console.log(error);
+//       res.redirect("/toolbox");
+//    }
+// });
 
 
 

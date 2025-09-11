77c32e17a5967e1c157bdf4d4a9f03d8f8355db9
Sara Mattina <smattina@gmail.com>
2025-09-11 11:39:58 -0400

    create edit userTool

--- controllers/toolbox.js
+++ controllers/toolbox.js
@@ -21,7 +21,6 @@ router.get("/myTools", async (req, res) => {
    try {
       const userTools = await UserTool.find({ user: req.session.user._id })
          .populate("tool");
-         console.log("All userTools:", userTools);
       res.render("toolbox/index.ejs", { userTools });
    } catch (error) {
       console.log(error);
@@ -36,12 +35,10 @@ router.get("/myTools/:userToolId", async (req, res) => {
          user: req.session.user._id,
       }).populate("tool");
 
-      console.log("Tool name:", userTool)
       if (!userTool) {
          console.log("usertool not found")
          return res.redirect("/toolbox/myTools")};
-      console.log("UserTool fetched:", userTool);
-      console.log("session user:", req.session.user);
+
       res.render("toolbox/show.ejs", { userTool });
    } catch (error) {
       console.log(error);
@@ -49,6 +46,38 @@ router.get("/myTools/:userToolId", async (req, res) => {
    }
 });
 
+router.get("/myTools/:userToolId/edit", async (req, res) => {
+   try {
+      const userTool = await UserTool.findOne({
+         _id: req.params.userToolId,
+         user: req.session.user._id,
+      }).populate("tool");
+      if (!userTool) {
+         return res.redirect("/toolbox/myTools")};
+      res.render("toolbox/edit.ejs", { userTool });
+   } catch (error) {
+      console.log(error);
+      res.redirect("/toolbox/myTools");
+   }
+});
+
+router.put("/myTools/:userToolId", async (req, res) => {
+   try {
+      const userTool = await UserTool.findOne({
+         _id: req.params.userToolId,
+         user: req.session.user._id,
+      }).populate("tool");
+      
+      userTool.set(req.body);
+      await userTool.save();
+
+      res.redirect(`/toolbox/myTools/${req.params.userToolId}`);
+
+   } catch (error) {
+      console.log(error);
+      res.redirect("/toolbox/myTools");
+   }
+});
 
 router.get("/:toolId", async (req, res) => {
    try {
